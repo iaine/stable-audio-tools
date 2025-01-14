5d653504edeb962ef6b37d3dee372082020c4d5a
Iain Emsley <Iain.Emsley@warwick.ac.uk>
2025-01-14 20:18:12 +0000

    Added export of tensor

--- stable_audio_tools/inference/generation.py
+++ stable_audio_tools/inference/generation.py
@@ -274,7 +274,7 @@ def generate_diffusion_cond(
     print(sampled)
     print(sampled.shape)
     # Return audio
-    return sampled
+    return (conditioning_tensors["prompt"][0][0], sampled)
 
 # builds a softmask given the parameters
 # returns array of values 0 to 1, size sample_size, where 0 means noise / fresh generation, 1 means keep the input audio, 
