34d4a3f151d7f1be87b9d7c86be253f2409df0c2
iaine <iain.emsley@kellogg.ox.ac.uk>
2025-03-28 14:10:25 +0000

    Fix type error

--- stable_audio_tools/inference/generation.py
+++ stable_audio_tools/inference/generation.py
@@ -185,7 +185,8 @@ def generate_diffusion_cond(
     #softwarekey.append({"name": "conditioning_tensors", "text": text, "keys": list(conditioning_tensors.keys()), "types": [type(k) for k in conditioning_tensors.keys() ]})
 
     conditioning_inputs = model.get_conditioning_inputs(conditioning_tensors)
-    softwaretrace["conditioning_inputs"]={"data":conditioning_inputs["prompt"].cpu().numpy().tolist(), "types": conditioning_inputs["prompt"]}
+    print(conditioning_inputs.keys())
+    softwaretrace["conditioning_inputs"]={"data":conditioning_inputs.cpu().numpy().tolist(), "types": conditioning_inputs}
     #stack = traceback.extract_stack()
     #(filename, line, procname, text) = stack[-1]
     #softwarekey.append({"name": "conditioning_tensors", "text": text, "keys": list(conditioning_inputs.keys()), "types": [type(k) for k in conditioning_inputs.keys() ]})
